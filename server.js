e8cddac9841459e234ffbc412e7e85e98ac3f0ee
Eric O'Brien <obrienjeric@gmail.com>
2021-10-12 13:12:58 -0400

    adding functionality and css

--- server.js
+++ server.js
@@ -12,23 +12,40 @@ const app = express();
 
 //set middleware to parse express calls into json
 app.use(express.json());
+
+//middleware to access css file
 app.use('/style', express.static(path.join(__dirname, './public/styles.css')));
 
+//middleware to access html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
     rollbar.info('html file server successfully');
 });
 
+
+//following is code to capture student names that are inputed on the webpage
+//student names are posted back onto the webpage as new elements
+//code in place to handle cases such as if someone doesn't enter a name
 let students = [];
 
 app.post('/api/student', (req, res) => {
     let { name } = req.body;
     name = name.trim();
 
-    students.push(name);
+    const index = students.findIndex(studentName=> studentName === name)
 
-    rollbar.log('student added sucessfully', {author: 'Dj', type: 'manual entry'});
-    res.status(200).send(students);
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Jim', type: 'manual'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
 })
 
 
