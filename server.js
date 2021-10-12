506a445de64f6baee9483ed7b9f8b42960968dd1
Eric O'Brien <obrienjeric@gmail.com>
2021-10-12 12:05:50 -0400

    adjusted PORT

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-const port = process.env.port || 4400;
+const port = process.env.PORT || 4400;
 
 app.listen(port, () => { console.log(`Server is up on ${port}.`)})
