d31d5f269d20eb708e9e9b4599ab822c45cd3042
Eric O'Brien <obrienjeric@gmail.com>
2021-10-12 12:14:48 -0400

    introduce rollbar

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
-const path = require('path')
-
+const path = require('path');
+const Rollbar = require('rollbar');
+const rollbar = new Rollbar( {
+    accessToken: 'd01a8fd4a27e49f38d7105e5cd66a4a2',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 const app = express();
 
 app.get('/', (req, res) => {
