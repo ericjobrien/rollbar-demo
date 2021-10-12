6bf9f20063322c0edcde48ca068f75003d6e3bf9
Eric O'Brien <obrienjeric@gmail.com>
2021-10-12 12:21:36 -0400

    rollbar

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const Rollbar = require('rollbar');
-const Rollbar = require('rollbar');
+
 const rollbar = new Rollbar( {
     accessToken: 'f6fcd322cca041139739f21ce8ca9d33',
     captureUncaught: true,
@@ -11,6 +11,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
+    rollbar.info('html file server successfully');
 });
 
 const port = process.env.PORT || 4400;
